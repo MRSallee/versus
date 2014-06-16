71af99bb56793a993093f7c63fc9f3ec2578a36d
mrsallee <markryansallee@yahoo.com>
2014-06-15 19:20:33 -0700

    Updated video URLs for final resting place

--- data/e3-2014.js
+++ data/e3-2014.js
@@ -21,31 +21,31 @@ var metadata = {
             name: 'Games',
             question: 'Who had the best <b>games</b>?',
             thumbnail: 'http://assets1.ignimgs.com/2014/06/16/versusgames1920jpg-6e4917_610w.jpg',
-            videoUrlNeutral: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-the-games-ign-versus',
-            videoUrlIntro: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-the-games-ign-versus'
+            videoUrlNeutral: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-the-games-ign-versus',
+            videoUrlIntro: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-the-games-ign-versus'
         },
         category2 : {
             name: 'Conferences',
             question: 'Who had the best <b>conference</b>?',
             thumbnail: 'http://assets2.ignimgs.com/2014/06/16/versusconf1920jpg-b68872_610w.jpg',
-            videoUrlNeutral: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-the-conferences-ign-versus',
-            videoUrlVote1: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-the-conferences-microsoft-games-vote-ign-versus',
-            videoUrlVote2: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-the-conferences-sony-games-vote-ign-versus',
-            videoUrlVote3: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-the-conferences-nintendo-games-vote-ign-versus'
+            videoUrlNeutral: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-the-conferences-ign-versus',
+            videoUrlVote1: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-the-conferences-microsoft-games-vote-ign-versus',
+            videoUrlVote2: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-the-conferences-sony-games-vote-ign-versus',
+            videoUrlVote3: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-the-conferences-nintendo-games-vote-ign-versus'
         },
         category3 : {
             name: 'Overall',
             question: 'Who won <b>E3 2014</b>?',
             thumbnail: 'http://assets2.ignimgs.com/2014/06/16/versusoverall1920jpg-6e4918_610w.jpg',
-            videoUrlNeutral: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-overall-ign-versus',
-            videoUrlVote1: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-overall-microsoft-conference-vote-ign-versus',
-            videoUrlVote2: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-overall-sony-conference-vote-ign-versus',
-            videoUrlVote3: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-overall-nintendo-conference-vote-ign-versus'
+            videoUrlNeutral: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-overall-ign-versus',
+            videoUrlVote1: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-overall-microsoft-conference-vote-ign-versus',
+            videoUrlVote2: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-overall-sony-conference-vote-ign-versus',
+            videoUrlVote3: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-overall-nintendo-conference-vote-ign-versus'
         },
         category4 : {
             name: 'Thanks',
-            videoUrlVote1: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-the-finale-microsoft-overall-vote-ign-versus',
-            videoUrlVote2: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-the-finale-sony-overall-vote-ign-versus',
-            videoUrlVote3: 'http://www.ign.com/videos/2014/05/15/who-won-e3-2014-the-finale-nintendo-overall-vote-ign-versus'
+            videoUrlVote1: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-the-finale-microsoft-overall-vote-ign-versus',
+            videoUrlVote2: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-the-finale-sony-overall-vote-ign-versus',
+            videoUrlVote3: 'http://www.ign.com/videos/2014/06/20/who-won-e3-2014-the-finale-nintendo-overall-vote-ign-versus'
         }
     };
